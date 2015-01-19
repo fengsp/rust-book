a535f11d541b6083ec5ff7f24f887d8252f308cf
Shipeng Feng <fsp261@gmail.com>
2015-01-19 18:44:27 +0800

    Added variable bindings

---
+++ basics/variable_bindings/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    let (a, b) = (1, 2);
+    let y: i32 = 5;
+    let mut z = 5;
+    z = z + 10;
+    let c: i32;
+    c = 5;
+    println!("The values are: {}{}{}{}{}{}", a, b, c, x, y, z);
+}
